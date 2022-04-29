e9eb11223feb72f8e84f3d3c59876f6022ca3965
it <email@e.com>
2022-04-29 18:30:05 +0300

    add description into READMY.md

--- fibo-server/src/main/java/com/server/fiboserver/controller/FibonacciClientController.java
+++ fibo-server/src/main/java/com/server/fiboserver/controller/FibonacciClientController.java
@@ -1,27 +1,7 @@
 package com.server.fiboserver.controller;
 
-import com.server.fiboserver.service.FibonacciSequence;
-import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import reactor.core.publisher.Flux;
 
-import java.math.BigInteger;
-import java.time.Duration;
-
-@Controller
-@RequiredArgsConstructor
 public class FibonacciClientController {
-
-    private final FibonacciSequence fibonacciSequence;
-
-    @MessageMapping("fibonacci.sequence")
-    public Flux<BigInteger> getSequenceFibonacci() {
-
-        int timeOut = 100;
-
-        return this.fibonacciSequence.generateFibonacciSequence()
-                .delayElements(Duration.ofMillis(timeOut));
-
-    }
 }
