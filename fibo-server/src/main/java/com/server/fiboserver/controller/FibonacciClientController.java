00613d6c7d560d9fa88730b35fd7889684be9a9d
it <email@e.com>
2022-05-01 22:54:41 +0300

    create server-RSocket

--- fibo-server/src/main/java/com/server/fiboserver/controller/FibonacciClientController.java
+++ fibo-server/src/main/java/com/server/fiboserver/controller/FibonacciClientController.java
@@ -1,7 +1,28 @@
 package com.server.fiboserver.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.server.fiboserver.service.FibonacciSequence;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
+import java.math.BigInteger;
+import java.time.Duration;
+
+@Controller
+@RequiredArgsConstructor
 public class FibonacciClientController {
+
+    private final FibonacciSequence fibonacciSequence;
+
+    @MessageMapping("fibonacci.sequence")
+    public Flux<BigInteger> getSequenceFibonacci() {
+
+        int timeOut = 100;
+
+        return
+                this.fibonacciSequence.generateFibonacciSequence()
+                        .delayElements(Duration.ofMillis(timeOut));
+
+    }
 }
